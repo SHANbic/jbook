5eb6a873486deb7c1fd9a587dbef3753132b9d6e
SHANbic <supagoof924@hotmail.com>
2021-12-23 20:33:43 +0100

    feat: implemented monaco code editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import "bulmaswatch/superhero/bulmaswatch.min.css";
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
+import CodeEditor from "./components/code-editor";
 
 const App = () => {
   const ref = useRef<any>();
@@ -62,9 +64,15 @@ const App = () => {
   `;
   return (
     <div>
+      <CodeEditor
+        initialValue={"const a = 1"}
+        onChange={(value) => {
+          setInput(value);
+        }}
+      />
       <textarea value={input} onChange={(e) => setInput(e.target.value)} />
       <div>
-        <button onClick={onClick}>Convert</button>
+        <button className="button button-format is-primary is-small" onClick={onClick}>Convert</button>
       </div>
       <iframe
         ref={iframe}
