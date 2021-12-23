a546ae575f18aa90cc8b7aebae5fe6a4ac56f149
SHANbic <supagoof924@hotmail.com>
2021-12-23 15:44:12 +0100

    feat: implemented code rendering inside a safe iframe sandbox

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
   const ref = useRef<any>();
+  const iframe = useRef<any>();
   const [input, setInput] = useState("");
-  const [code, setCode] = useState("");
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -24,6 +24,9 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -34,16 +37,41 @@ const App = () => {
         global: "window",
       },
     });
-    //console.log(result)
-    setCode(result.outputFiles[0].text);
+
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+  <html>
+    <head></head>
+    <body>
+      <div id="root"></div>
+      <script>
+        window.addEventListener('message', (event)=>{
+          try {
+            eval(event.data)
+          } catch (err) {
+            const root = document.querySelector("#root");
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+            console.error(err);
+          }
+        }, false)
+      </script>
+    </body>
+  </html>
+  `;
   return (
     <div>
       <textarea value={input} onChange={(e) => setInput(e.target.value)} />
       <div>
         <button onClick={onClick}>Convert</button>
       </div>
-      <pre>{code}</pre>
+      <iframe
+        ref={iframe}
+        title="preview"
+        sandbox="allow-scripts"
+        srcDoc={html}
+      ></iframe>
     </div>
   );
 };
