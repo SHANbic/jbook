92f8ce78cd60fb5a01164348dec2b7c30b735043
SHANbic <supagoof924@hotmail.com>
2021-12-24 16:32:51 +0100

    feat: handled resizable editor window

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,11 +1,17 @@
+import './preview.css';
 import { useRef, useEffect } from "react";
+
 interface PreviewProps {
   code: string;
 }
 
 const html = `
   <html>
-    <head></head>
+    <head>
+      <style>
+        html {background-color: white;}
+      </style>
+    </head>
     <body>
       <div id="root"></div>
       <script>
@@ -28,15 +34,20 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, "*");
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, "*");
+    }, 50)
   }, [code]);
+
   return (
-    <iframe
+    <div className="preview-wrapper">
+      <iframe
       ref={iframe}
       title="preview"
       sandbox="allow-scripts"
       srcDoc={html}
-    />
+      />
+    </div>
   );
 };
 
--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,35 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CodeEditor from "./code-editor";
-import Preview from "./Preview";
+// @ts-ignore
+import Preview from "./preview";
 import bundle from "../bundler";
+import Resizable from "./resizable";
 
 const CodeCell = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(()=>{
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 800)
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [input])
 
   return (
-    <div>
-      <CodeEditor
+      <Resizable direction="vertical">
+    <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+      <Resizable direction="horizontal">
+        <CodeEditor
         initialValue={"const a = 1"}
         onChange={(value) => {
           setInput(value);
         }}
       />
+      </Resizable>
       <div>
-        <button
-          className="button button-format is-primary is-small"
-          onClick={onClick}
-        >
-          Convert
-        </button>
       </div>
       <Preview code={code} />
     </div>
+    </Resizable>
   );
 };
 
