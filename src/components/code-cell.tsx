2f3d0826ca3f0b42b6051498b48bcf800bedda1d
SHANbic <supagoof924@hotmail.com>
2021-12-27 10:39:22 +0100

    feat: made use of immer for state udpates in store

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -22,18 +22,17 @@ const CodeCell = () => {
     };
   }, [input]);
 
-  const initialValue = `import React from "react";
-import ReactDOM from "react-dom";
+  const initialValue = `import React from 'react';
+import ReactDOM from 'react-dom';
 
 const App = () => {
   return (
-      <>
-        <h1>Hello World!</h1>
-      </>
-    )
-}
-ReactDOM.render(<App />, document.querySelector("#root"));
-  `;
+    <>
+      <h1>Hello World!</h1>
+    </>
+  );
+};
+ReactDOM.render(<App />, document.querySelector('#root'));`;
 
   return (
     <Resizable direction="vertical">
